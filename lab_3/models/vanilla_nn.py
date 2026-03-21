37a45b1486b02fee483ea5a28c296dfc782f841b
andrewhall1124 <andrewmartinhall2@gmail.com>
2026-03-21 09:34:32 -0600

    Finished variational inference, updated vanilla nn.

--- lab_3/models/vanilla_nn.py
+++ lab_3/models/vanilla_nn.py
@@ -3,6 +3,7 @@ from torch import nn
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tqdm import trange
 
 # Build model
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
@@ -13,8 +14,6 @@ class VanillaNN(nn.Module):
         super().__init__()
         self.model = nn.Sequential(
             nn.Linear(1, 64),
-            nn.ReLU(),
-            nn.Linear(64, 64),
             nn.ReLU(),
             nn.Linear(64, 64),
             nn.ReLU(),
@@ -38,7 +37,7 @@ y_train = train['y']
 X_train = torch.tensor(X_train, dtype=torch.float32).unsqueeze(1).to(device)
 y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(1).to(device)
 
-for epoch in range(1000):
+for epoch in trange(1000):
     y_hat = model(X_train)
 
     loss = criterion(y_hat, y_train)
@@ -49,28 +48,36 @@ for epoch in range(1000):
 
 # Testing
 test = pd.read_csv("data/test-1.csv")
-X_test = test['X']
-y_test = test['y']
-
-X_test = torch.tensor(X_test, dtype=torch.float32).unsqueeze(1).to(device)
+X_test = torch.tensor(test['X'], dtype=torch.float32).unsqueeze(1).to(device)
+y_test = torch.tensor(test['y'], dtype=torch.float32).unsqueeze(1).to(device)
 
 with torch.no_grad():
-    y_pred = model(X_test)
+    y_test_pred = model(X_test)
+    rmse = torch.sqrt(torch.mean((y_test_pred - y_test) ** 2)).item()
+
+# Predictions over linspace
+lb, ub = test['X'].min(), test['X'].max()
+X_lin = torch.linspace(lb, ub, 200).unsqueeze(1).to(device)
+
+with torch.no_grad():
+    y_lin = model(X_lin)
 
 pred = pd.DataFrame({
-    'X': X_test.cpu().squeeze().numpy(),
-    'y': y_pred.cpu().squeeze().numpy()
+    'X': X_lin.cpu().squeeze().numpy(),
+    'y': y_lin.cpu().squeeze().numpy()
 })
 
-lb, ub = train['X'].min(), train['X'].max()
+train_lb, train_ub = train['X'].min(), train['X'].max()
 
 sns.scatterplot(train, x='X', y='y', label='TRAIN', alpha=.5)
 sns.scatterplot(test, x='X', y='y', label='TEST', alpha=.5)
-sns.lineplot(pred, x='X', y='y', label='Predicitions', color='green')
+sns.lineplot(pred, x='X', y='y', label='Predictions', color='green')
 
-plt.axvline(x=lb, color='red', linestyle='--', label='Training Data Ends')
-plt.axvline(x=ub, color='red', linestyle='--', label='Training Data Ends')
+plt.axvline(x=train_lb, color='red', linestyle='--')
+plt.axvline(x=train_ub, color='red', linestyle='--', label='Training Data Ends')
 
+plt.suptitle("Vanilla NN")
+plt.title(f"RMSE (test): {rmse:.4f}")
 plt.legend()
 
 plt.savefig("results/vanilla_nn.png", dpi=300)
